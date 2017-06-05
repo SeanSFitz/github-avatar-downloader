c72583e0e53ed9c1808a893232fa83d1ea8f30c3
SeanSFitz <sean.s.fitz@gmail.com>
2017-06-05 16:55:20 +0000

    works for one avatar

--- download-avatars.js
+++ download-avatars.js
@@ -1,6 +1,5 @@
 var request = require('request');
-
-console.log(`Welcome to the GitHub Avatar Downloader!`);
+var fs = require('fs');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
@@ -19,10 +18,22 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 };
 
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function (err) {
+      throw err;
+    })
+    .on('response', function (response) {
+      console.log("Avatar downloaded")
+    })
+    .pipe(fs.createWriteStream(filePath));
+};
+
 var GITHUB_USER = "SeanSFitz";
 var GITHUB_TOKEN = "7880bc40d0ea572eb4fd38f5a85d8fa0e142e858";
 
-
+console.log(`Welcome to the GitHub Avatar Downloader!`);
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
@@ -30,3 +41,5 @@ getRepoContributors("jquery", "jquery", function(err, result) {
     console.log(`${contributor.avatar_url}`);
   }
 });
+
+downloadImageByURL("https://avatars3.githubusercontent.com/u/1615?v=3", "test");
